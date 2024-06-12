5169526439747cbb80c84589b3a7ce7a1c10a092
Sveppg <terminator12327@gmail.com>
2024-06-12 20:21:28 +0200

    new form

--- sinform.m
+++ sinform.m
@@ -1,9 +1,30 @@
-clc
-clearvars
+clc;
+clearvars;
 
-x = -10:0.1:10;
-plot (x, sin (x));
-xlabel ("x");
-ylabel ("sin (x)");
-title ("Simple 2-D Plot");
+% Parameter
+f = 50; % Frequenz in Hz
+T = 1 / f; % Periode in Sekunden
+omega = 2 * pi * f; % Winkelgeschwindigkeit
+t = linspace(0, 2 * T, 1000); % Zeitvektor über zwei Perioden
 
+% Spannung und Strom
+U0 = 1; % Maximale Spannung (Amplitude)
+I0 = 1; % Maximaler Strom (Amplitude)
+
+% Spannungskurve (Sinusfunktion)
+u = U0 * sin(omega * t);
+
+% Stromkurve (Sinusfunktion, um 90° phasenverschoben)
+i = I0 * sin(omega * t - pi / 2);
+
+% Plot
+figure;
+hold on;
+plot(t, u, 'b', 'DisplayName', 'Spannung u(t)');
+plot(t, i, 'r', 'DisplayName', 'Strom i(t)');
+xlabel('Zeit (s)');
+ylabel('Amplitude');
+title('Spannung und Strom eines induktiven Blindwiderstands');
+legend;
+grid on;
+hold off;
